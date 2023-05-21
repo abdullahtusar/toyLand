41a2ce9fbabfcf294c2a429f91865147f0dc3263
abdullahtusar <abdullahtusar2@gmail.com>
2023-05-21 08:38:31 +0600

    added AOS successfully

--- src/pages/Home/Category/SubCategory3.jsx
+++ src/pages/Home/Category/SubCategory3.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-
-const SubCategory3 = ({toy}) => {
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+const SubCategory3 = ({ toy }) => {
     const { title, category, photo, price, rating } = toy;
+    useEffect(() => {
+        AOS.init({ duration: 1000 })
+    }, [])
     return (
         <>
             <div>
                 {
                     category && category === "transformers" ? <div className="card w-60 md:w-96 bg-base-100 shadow-xl border-4 border-gray-300">
-                        <figure><img src={photo} alt="Shoes" /></figure>
+                        <figure>
+                            <div data-aos="flip-left">
+                                <img src={photo} alt="Shoes" />
+                            </div>
+                        </figure>
                         <div className="card-body">
                             <h2 className="card-title">{title}</h2>
                             <div className='flex justify-between'>
                                 <div>
-                                    <p>Price: {price }$</p>
-                                    <p>Rating: { rating }</p>
+                                    <p>Price: {price}$</p>
+                                    <p>Rating: {rating}</p>
                                 </div>
                                 <div className="card-actions justify-end">
                                     <button className="btn bg-red-600 border-0">Details</button>
